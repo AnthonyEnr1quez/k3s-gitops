4d7b777ef3a08b8d3bfc812ed422ef2c5a130446
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2025-06-29 01:29:38 -0500

    add korb to flake

--- flake.nix
+++ flake.nix
@@ -6,7 +6,21 @@
 
   outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        korb = pkgs.buildGoModule rec {
+          pname = "korb";
+          version = "2.3.2";
+          src = pkgs.fetchFromGitHub {
+            owner = "BeryJu";
+            repo = "${pname}";
+            rev = "v${version}";
+            sha256 = "sha256-4f5Ii75Pc+88ONiEWAQG7A6NaKvQ5/1jqu3O0f7AaVI=";
+          };
+          doCheck = false;
+          vendorHash = "sha256-ZXepf1gWdw9H/5XV3JPuyN8X9LG5C9La5oRLjtsRa0s=";
+        };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -24,6 +38,7 @@
             regctl
             cmctl
             restic
+            korb
           ];
         };
 
