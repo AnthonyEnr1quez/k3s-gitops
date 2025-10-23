3ebda8001de955f488fc613c70d1057868c7f121
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2025-10-23 09:12:26 -0500

    add cloudflared to flake

--- flake.nix
+++ flake.nix
@@ -27,6 +27,7 @@
           default = pkgs.mkShell {
             packages = with pkgs; [
               age
+              cloudflared
               fluxcd
               go-task
               kubectl
